5f751776945786ba5776c602acb420581f5bd83c
Jonathan Fok kan <jfokkan@gmail.com>
2021-04-28 17:40:21 -0400

    Added basic log

--- src/log.rs
+++ src/log.rs
@@ -1,27 +1,80 @@
-use anyhow::Result;
-use git2::{Commit, Repository, Time};
+use crate::error::GitDataStoreError;
 use chrono::{FixedOffset, TimeZone};
+use git2::{Commit, Oid, Repository, Time};
+use serde::Serialize;
 
-pub fn git_log(repo: Repository) -> Result<()> {
+#[derive(Serialize)]
+pub struct History {
+    commits: Vec<HistoryEntry>,
+}
+
+#[derive(Serialize)]
+pub struct HistoryEntry {
+    timestamp_seconds: i64,
+    commit_id: String,
+    author: String,
+    stats: HistoryStats,
+}
+
+#[derive(Serialize)]
+pub struct HistoryStats {
+    files_changed: usize,
+    insertions: usize,
+    deletions: usize,
+}
+
+pub fn git_log(repo: &Repository, skip: usize, limit: usize) -> Result<History, GitDataStoreError> {
     let mut rev_walk = repo.revwalk()?;
     rev_walk.push_head()?;
 
-    for rev in rev_walk {
-        let rev = rev?;
-        let commit = repo.find_commit(rev)?;
-        println!("{}", print_commit(commit)?);
-    }
-    Ok(())
+    let history: Result<Vec<HistoryEntry>, GitDataStoreError> = rev_walk
+        .into_iter()
+        .skip(skip)
+        .enumerate()
+        .take_while(|(i, _rev)| *i < limit)
+        .map(|(_i, rev)| map_rev(repo, rev))
+        .collect();
+
+    Ok(History { commits: history? })
 }
 
-pub fn print_commit(commit: Commit) -> Result<String> {
-    Ok(format!(
+fn map_rev(
+    repo: &Repository,
+    rev: Result<Oid, git2::Error>,
+) -> Result<HistoryEntry, GitDataStoreError> {
+    let rev = rev?;
+
+    let commit = repo.find_commit(rev)?;
+    let commit_tree = commit.tree()?;
+
+    let parent = commit.parents().next();
+    let parent_tree = parent.map(|p| p.tree()).transpose()?;
+
+    let diff = repo.diff_tree_to_tree(parent_tree.as_ref(), Some(&commit_tree), None)?;
+    let diff_stats = diff.stats()?;
+    let stats = HistoryStats {
+        files_changed: diff_stats.files_changed(),
+        insertions: diff_stats.insertions(),
+        deletions: diff_stats.deletions(),
+    };
+
+    let x = Ok(HistoryEntry {
+        timestamp_seconds: commit.time().seconds(),
+        commit_id: commit.id().to_string(),
+        author: commit.author().to_string(),
+        stats: stats,
+    });
+    x
+}
+
+pub fn print_commit(commit: Commit) -> String {
+    format!(
         "commit {}\nAuthor: {}\nDate: {}\n\n{}\n",
         commit.id().to_string(),
         commit.author().to_string(),
         print_commit_time(&commit.time()),
         commit.summary().unwrap_or(""),
-    ))
+    )
 }
 
 pub fn print_commit_time(time: &Time) -> String {
